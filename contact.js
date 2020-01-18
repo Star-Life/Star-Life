120562d7608c5bafbd69fbc7ceea6d5d6f778002
Star-Life <59966920+Star-Life@users.noreply.github.com>
2020-01-18 23:10:40 +0000

    Update contact.js

--- contact.js
+++ contact.js
@@ -75,10 +75,10 @@ function showPosition(position) {
 + position.coords.longitude +
 "!4m5!1s0xfc5483436555a4d%3A0xe25bba5cbd9b468f!2sberekum!3m2!1d"
 + position.coords.latitude +
-"!2d"
+"!2d-"
 + position.coords.longitude +
 "!5e0!3m2!1sen!2suk!4v1579365049470!5m2!1sen!2suk\"  "
 	+ "width=\"600\" height=\"450\" frameborder=\"0\" style=\"border:0;\""
 	document.getElementById("google-map").innerHTML = "<"+y+"></iframe>";
-		document.getElementById("map-title").innerHTML = "EEEE   "+ y +"   EEEE";	
+		document.getElementById("map-title").innerHTML = "GGGG   "+ y +"   GGGG";	
 }
