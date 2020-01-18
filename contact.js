49048a7fee3e6df136bad5c818053a6b6f20ad8e
Star-Life <59966920+Star-Life@users.noreply.github.com>
2020-01-18 23:38:35 +0000

    Update contact.js

--- contact.js
+++ contact.js
@@ -67,7 +67,7 @@ function getLocation() {
 */
 function showPosition(position) {
 	document.getElementById("map-title").innerHTML = "The recommended route from your location to us, is below:";	
-	y="iframe src=\"https://www.google.com/maps/embed?pb=!1m28!1m12!1m3!1d253223.8587458276!2d-2.5884569446603813!3d7.396094138143066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m5!2s"
+	y="iframe src=\"https://www.google.com/maps/embed?pb=!1m27!1m12!1m3!1d253223.8587458276!2d-2.5884569446603813!3d7.396094138143066!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m13!3e6!4m4!2s"
 + position.coords.latitude + position.coords.longitude +
 "!3m2!1d"
 + position.coords.latitude +
@@ -80,5 +80,5 @@ function showPosition(position) {
 "!5e0!3m2!1sen!2suk!4v1579365049470!5m2!1sen!2suk\"  "
 	+ "width=\"600\" height=\"450\" frameborder=\"0\" style=\"border:0;\""
 	document.getElementById("google-map").innerHTML = "<"+y+"></iframe>";
-		document.getElementById("map-title").innerHTML = "GGGG   "+ y +"   GGGG";	
+		document.getElementById("map-title").innerHTML = "JJJJ   "+ y +"   JJJJ";	
 }
